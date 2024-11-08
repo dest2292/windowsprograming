d8b2105f2848b0917d549cf416c8071e19ad29d1
dest2292 <cs20212292@gmail.com>
2024-11-08 16:22:29 +0900

    Add files via upload

---
+++ MP3_v1.2/MP3/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace MP3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Play = new System.Windows.Forms.Button();
+            this.Pause = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.Open = new System.Windows.Forms.Button();
+            this.Close = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.MP3TrackBar = new System.Windows.Forms.TrackBar();
+            this.MP3Timer = new System.Windows.Forms.Timer(this.components);
+            this.LB_MusicTimer = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.MP3TrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Play
+            // 
+            this.Play.Location = new System.Drawing.Point(61, 118);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(25, 25);
+            this.Play.TabIndex = 1;
+            this.Play.Text = "▶";
+            this.Play.UseVisualStyleBackColor = true;
+            this.Play.Click += new System.EventHandler(this.Play_Click);
+            // 
+            // Pause
+            // 
+            this.Pause.Location = new System.Drawing.Point(122, 118);
+            this.Pause.Name = "Pause";
+            this.Pause.Size = new System.Drawing.Size(25, 25);
+            this.Pause.TabIndex = 2;
+            this.Pause.Text = "ll";
+            this.Pause.UseVisualStyleBackColor = true;
+            this.Pause.Click += new System.EventHandler(this.Pause_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(181, 118);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(25, 25);
+            this.Stop.TabIndex = 3;
+            this.Stop.Text = "■";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Open
+            // 
+            this.Open.Location = new System.Drawing.Point(250, 120);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(75, 23);
+            this.Open.TabIndex = 5;
+            this.Open.Text = "Open";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // Close
+            // 
+            this.Close.Location = new System.Drawing.Point(250, 154);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(75, 23);
+            this.Close.TabIndex = 6;
+            this.Close.Text = "Close";
+            this.Close.UseVisualStyleBackColor = true;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Play       Pause       Stop";
+            // 
+            // MP3TrackBar
+            // 
+            this.MP3TrackBar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.MP3TrackBar.Location = new System.Drawing.Point(49, 56);
+            this.MP3TrackBar.Maximum = 999999;
+            this.MP3TrackBar.Name = "MP3TrackBar";
+            this.MP3TrackBar.Size = new System.Drawing.Size(285, 45);
+            this.MP3TrackBar.TabIndex = 8;
+            this.MP3TrackBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MP3TrackBar_MouseDown);
+            this.MP3TrackBar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MP3TrackBar_MouseMove);
+            this.MP3TrackBar.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MP3TrackBar_MouseUp);
+            // 
+            // MP3Timer
+            // 
+            this.MP3Timer.Interval = 20;
+            this.MP3Timer.Tick += new System.EventHandler(this.MP3Timer_Tick);
+            // 
+            // LB_MusicTimer
+            // 
+            this.LB_MusicTimer.Location = new System.Drawing.Point(225, 30);
+            this.LB_MusicTimer.Name = "LB_MusicTimer";
+            this.LB_MusicTimer.Size = new System.Drawing.Size(100, 23);
+            this.LB_MusicTimer.TabIndex = 10;
+            this.LB_MusicTimer.Text = "00:00";
+            this.LB_MusicTimer.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(380, 241);
+            this.Controls.Add(this.LB_MusicTimer);
+            this.Controls.Add(this.Play);
+            this.Controls.Add(this.MP3TrackBar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.Open);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Pause);
+            this.Name = "Form1";
+            this.Text = "MP3 Player";
+            ((System.ComponentModel.ISupportInitialize)(this.MP3TrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Play;
+        private System.Windows.Forms.Button Pause;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.Button Close;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar MP3TrackBar;
+        private System.Windows.Forms.Timer MP3Timer;
+        private System.Windows.Forms.Label LB_MusicTimer;
+    }
+}
+
---
+++ MP3_v1.2/MP3/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MP3
+{
+    public partial class Form1 : Form
+    {
+        MP3Player mp3Player;
+
+        //Track Bar 
+        bool isScrolled = false;
+        int trackBarBlankSize = 14;    // TrackBar 양옆 빈공간
+        int trackBarLength = 0;        // TrackBar의 실제 길이
+        int trackBarMouseX = 0;        // TrackBar에서 마우스 클릭 위치
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            mp3Player = new MP3Player();
+            trackBarLength = MP3TrackBar.Size.Width - (trackBarBlankSize * 2); // TrackBar의 실제 길이
+        }
+
+        // Timer Tick Event
+        private void MP3Timer_Tick(object sender, EventArgs e) 
+        {
+            if (!mp3Player.isOpened)
+                return;
+
+            if (isScrolled == false)
+                MP3TrackBar.Value = mp3Player.GetPosition();                     
+
+            if (!mp3Player.loop && mp3Player.GetPosition() == mp3Player.GetLength())
+                mp3Player.Stop();
+
+            SetMusicTimer();
+        }
+
+        // 파일 열기
+        private void Open_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog open = new OpenFileDialog())
+            {
+                open.Filter = "Mp3 File|*.mp3";
+
+                open.InitialDirectory = Environment.CurrentDirectory;
+
+                if(open.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    string fileName = open.FileName;
+
+                    mp3Player.Open(fileName);
+                    MP3TrackBar.Maximum = mp3Player.GetLength();
+                    MP3Timer.Enabled = true;
+                }
+            }
+        }
+
+        // 파일 닫기
+        private void Close_Click(object sender, EventArgs e)
+        {
+            MP3Timer.Enabled = false;
+            MP3TrackBar.Value = 0;
+            mp3Player.Close();          
+        }
+
+        // 음악 재생 : ▶ Button
+        private void Play_Click(object sender, EventArgs e)
+        {        
+            mp3Player.Play();                
+        }
+
+        // 음악 일시정지 : ∥ Button
+        private void Pause_Click(object sender, EventArgs e)
+        {
+            mp3Player.Pause();
+        }
+
+        // 음악 정지 : ■ Button
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            mp3Player.Stop();
+        }
+
+        // TrackBar MouseDown Event
+        private void MP3TrackBar_MouseDown(object sender, MouseEventArgs e) 
+        {
+            isScrolled = true;
+            trackBarMouseX = e.X - trackBarBlankSize;     // 마우스 클릭 좌표
+            SetPositionByMouse(trackBarMouseX);
+        }
+
+        // TrackBar MouseMove Event
+        private void MP3TrackBar_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(isScrolled)
+            {
+                trackBarMouseX = e.X - trackBarBlankSize; // 마우스 클릭 좌표
+                SetPositionByMouse(trackBarMouseX);
+            }
+        }
+
+        // TrackBar MouseUp Event
+        private void MP3TrackBar_MouseUp(object sender, MouseEventArgs e)
+        {
+            if(mp3Player.isOpened)
+            {
+                string status = mp3Player.GetStatus();
+
+                mp3Player.Seek(MP3TrackBar.Value);
+
+                if (status == "playing")                   
+                    mp3Player.Play();
+                else
+                    mp3Player.Pause();
+            }
+
+            isScrolled = false;
+        }    
+
+        // TrackBar ▼ 이렇게 생긴애(클릭해서 끌어당길 수 있는 애) 트랙바 클릭시 마우스 따라가게 하는 메소드
+        private void SetPositionByMouse(int position)
+        {
+            if (position < 0 || position > trackBarLength)
+                return;
+
+            float rate = (float)position / (float)trackBarLength;
+            MP3TrackBar.Value = (int)(rate * (float)(MP3TrackBar.Maximum - MP3TrackBar.Minimum));
+        }
+
+
+        //음악 타이머 메소드
+        private void SetMusicTimer()
+        {
+            if (mp3Player.isOpened)
+            {
+                TimeSpan t = TimeSpan.FromMilliseconds(mp3Player.GetPosition());
+                LB_MusicTimer.Text = string.Format("{0:D2}:{1:D2}:{2:D3}", t.Minutes, t.Seconds, t.Milliseconds);
+            }
+        }
+    }
+}
